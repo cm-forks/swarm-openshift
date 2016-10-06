d80bfd05339c9831551d99d3e72546d587136b92
Charles Moulliard <cmoulliard@apache.org>
2016-10-06 13:48:09 +0200

    Add bundle dependencies

--- src/main/java/io/swagger/demo/RestServiceApp.java
+++ src/main/java/io/swagger/demo/RestServiceApp.java
@@ -3,15 +3,16 @@ package io.swagger.demo;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Set;
 
-//import org.jboss.logging.Logger;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.cdi.CDIFraction;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
 import org.wildfly.swarm.jaxrs.JAXRSFraction;
 import org.wildfly.swarm.logging.LoggingFraction;
-import org.wildfly.swarm.spi.api.StageConfig;
+
+//import org.jboss.logging.Logger;
 
 public class RestServiceApp {
     public static void main(String[] args) throws Exception {
@@ -25,6 +26,12 @@ public class RestServiceApp {
         System.out.println("Content : " + Files.readAllLines(Paths.get("/app/config/project.yml")));
 
         Swarm swarm = new Swarm().withStageConfig(cfg);
+        Set<String> keys = swarm.stageConfig().keys();
+        for(String key : keys) {
+            System.out.println("Key : " + key);
+            System.out.println("Value : " + swarm.stageConfig().resolve(key).getValue());
+        }
+
         swarm.fraction(new JAXRSFraction())
              .fraction(new CDIFraction())
              .fraction(new LoggingFraction());
