fb0e7463087bfb835afd1722aa4dc95bd2b533e5
Charles Moulliard <cmoulliard@apache.org>
2016-10-06 19:37:32 +0200

    Add @RunAsClient

--- swarm-rest/src/test/java/io/swarm/demo/JAXRSArquillianTest.java
+++ swarm-rest/src/test/java/io/swarm/demo/JAXRSArquillianTest.java
@@ -1,29 +1,21 @@
 package io.swarm.demo;
 
 import java.io.File;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.asset.FileAsset;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.swarm.Swarm;
-import org.wildfly.swarm.arquillian.CreateSwarm;
-import org.wildfly.swarm.cdi.CDIFraction;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
-import org.wildfly.swarm.jaxrs.JAXRSFraction;
-import org.wildfly.swarm.logging.LoggingFraction;
 
 @RunWith(Arquillian.class)
+@RunAsClient
 public class JAXRSArquillianTest extends SimpleHttp {
 
     @Deployment(testable = false)
@@ -35,7 +27,8 @@ public class JAXRSArquillianTest extends SimpleHttp {
         return deployment;
     }
 
-/*  @CreateSwarm
+    /*
+    @CreateSwarm
     public static Swarm newContainer() throws Exception {
         URL cfg = JAXRSArquillianTest.class.getClassLoader().getResource("project-stages.yml");
         System.out.println("URL : " + cfg.getFile());
@@ -43,10 +36,10 @@ public class JAXRSArquillianTest extends SimpleHttp {
                 .withStageConfig(cfg)
                 .fraction(new JAXRSFraction())
                 .fraction(LoggingFraction.createDebugLoggingFraction());
-    }*/
+    }
+    */
 
     @Test
-    @RunAsClient
     public void testResource() {
         // Call the /say/hello service and checks that we get the correct response
         Response response = getUrlContents("http://localhost:8080/say/hello");
