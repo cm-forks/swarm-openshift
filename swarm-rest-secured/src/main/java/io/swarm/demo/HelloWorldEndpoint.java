ba548d127733f9bf616b9ceb430d2f9eadbef491
Charles Moulliard <cmoulliard@apache.org>
2016-10-13 13:10:00 +0200

    Tested successfully using war package

--- swarm-rest-secured/src/main/java/io/swarm/demo/HelloWorldEndpoint.java
+++ swarm-rest-secured/src/main/java/io/swarm/demo/HelloWorldEndpoint.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -16,7 +14,6 @@ import javax.ws.rs.core.Response;
 
 import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
 
-@ApplicationScoped
 @Path("/say")
 public class HelloWorldEndpoint {
 
@@ -27,9 +24,7 @@ public class HelloWorldEndpoint {
         users = Arrays.asList(engineers.split(","));
     }
 
-    @Inject
-    @ConfigurationValue("endpoint.hello.message")
-    String message;
+    String message = "Hello from Secured endpoint";
 
     @GET
     @Produces("text/plain")
