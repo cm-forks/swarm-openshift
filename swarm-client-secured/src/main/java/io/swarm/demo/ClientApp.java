9d61901c6dc88f82611cd5301b3c201d8f91e010
Charles Moulliard <cmoulliard@apache.org>
2016-10-13 15:47:49 +0200

    Configure client to use BASIC AUTH

--- swarm-client-secured/src/main/java/io/swarm/demo/ClientApp.java
+++ swarm-client-secured/src/main/java/io/swarm/demo/ClientApp.java
@@ -1,40 +1,24 @@
 package io.swarm.demo;
 
-import java.io.IOException;
-import java.net.URL;
-
-import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.ClientRequestContext;
-import javax.ws.rs.client.ClientRequestFilter;
-import javax.ws.rs.client.ClientResponseContext;
-import javax.ws.rs.client.ClientResponseFilter;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.jboss.logging.Logger;
-import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.representations.AccessTokenResponse;
-
-import static org.keycloak.admin.client.resource.BearerAuthFilter.AUTH_HEADER_PREFIX;
+import org.apache.commons.codec.binary.Base64;
 
 public class ClientApp {
 
-    private final static String KEYCLOAK_URL = "http://localhost:8180/auth";
-    private final static String REALM = "basic-auth";
-    private final static String CLIENTID = "basic-auth-service";
     private final static String USER = "user";
-    private final static String PWD = "password";
+    private final static String PWD = "pwd";
 
     public static void main(String[] args) {
         Client client = ClientBuilder.newClient();
         Response response = client.target("http://localhost:8080/demo/say/echo")
                                   .queryParam("value","hello")
                                   .request()
-                                  .header(HttpHeaders.AUTHORIZATION,getToken())
+                                  .header(HttpHeaders.AUTHORIZATION,"Base " + getUserPwdEncoded(USER,PWD))
                                   .accept(MediaType.TEXT_PLAIN_TYPE)
                                   .get();
         System.out.println("Status " + response.getStatus());
@@ -42,7 +26,12 @@ public class ClientApp {
         response.close();
     }
 
-    public static String getToken() {
+    public static String getUserPwdEncoded(String user, String pwd) {
+        String val = user + ":" + pwd;
+        return Base64.encodeBase64String(val.getBytes());
+    }
+
+/*    public static String getToken() {
         AccessTokenResponse token = null;
         Keycloak keycloak = null;
         try {
@@ -66,6 +55,6 @@ public class ClientApp {
         }
 
         return AUTH_HEADER_PREFIX + token;
-    }
+    }*/
 
 }
