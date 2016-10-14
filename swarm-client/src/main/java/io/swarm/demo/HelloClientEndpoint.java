f3863d050ef910ccb468e682c4653ec2cc774c56
Charles Moulliard <cmoulliard@apache.org>
2016-10-14 22:56:48 +0200

    Add @Servicename annotation but I get a null response

--- swarm-client/src/main/java/io/swarm/demo/HelloClientEndpoint.java
+++ swarm-client/src/main/java/io/swarm/demo/HelloClientEndpoint.java
@@ -1,5 +1,10 @@
 package io.swarm.demo;
 
+import io.fabric8.annotations.ServiceName;
+import io.fabric8.annotations.External;
+import io.fabric8.annotations.Protocol;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,18 +16,25 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/say")
+@ApplicationScoped
 public class HelloClientEndpoint {
 
     private final static String USER = "user";
     private final static String PWD = "password";
+
+    @Inject
+    @Protocol("http") @ServiceName("swarm-rest")
+    private String helloService;
 
     @GET
     @Produces("text/plain")
     @Path("hello")
     public Response callHello() {
         System.out.println(">>> Calling the REST Service");
+        System.out.println(">>> Hello Service : " + helloService);
+        // String helloService = "http://localhost:8080" + "/say/echo";
         Client client = ClientBuilder.newClient();
-        Response response = client.target("http://localhost:8080/say/echo")
+        Response response = client.target(helloService + "/say/hello")
                 .queryParam("value","hello")
                 .request()
                 .accept(MediaType.TEXT_PLAIN_TYPE)
