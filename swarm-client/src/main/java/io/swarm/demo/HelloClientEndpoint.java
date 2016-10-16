e9df5d4ef4b35c1a04176ffd571921950a72ceb8
Charles Moulliard <cmoulliard@apache.org>
2016-10-16 17:04:06 +0200

    Remove string not used like the queryParam

--- swarm-client/src/main/java/io/swarm/demo/HelloClientEndpoint.java
+++ swarm-client/src/main/java/io/swarm/demo/HelloClientEndpoint.java
@@ -16,9 +16,6 @@ import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
 @ApplicationScoped
 public class HelloClientEndpoint {
 
-    private final static String USER = "user";
-    private final static String PWD = "password";
-
     @Inject
     @ConfigurationValue("service.hello.url")
     private String helloService;
@@ -28,10 +25,9 @@ public class HelloClientEndpoint {
     @Path("hello")
     public Response callHello() {
         System.out.println(">>> Calling the Hello REST Service");
-        System.out.println(">>> Hello Service : " + helloService);
+        System.out.println(">>> URL Hello Service : " + helloService);
         Client client = ClientBuilder.newClient();
         Response response = client.target(helloService + "/say/hello")
-                .queryParam("value","hello")
                 .request()
                 .accept(MediaType.TEXT_PLAIN_TYPE)
                 .get();
